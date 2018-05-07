f7f2b3cf0061b2f578e53b9f1bb7113cb6961aa9
Korawit Prutsachainimmit <korawit@Korawits-MacBook-Pro.local>
2018-05-07 18:16:23 +0700

    Add product api

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('pins.json')
+const router = jsonServer.router('products.json')
 const port = process.env.PORT || 3000
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
 server.listen(port, function () {
-  console.log('PSU Pin Server is running')
+  console.log('Json-server is running')
 })
